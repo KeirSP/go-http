f063babb082f76ed6f0c342453245e86902c35e9
ksitigarbha <58754415+karmicdribble@users.noreply.github.com>
2020-10-16 18:40:37 +0100

    initial commit

---
+++ http-server-basic-authentication.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	connHost = "localhost"
+	connPort = "8080"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+func main() {
+	http.HandleFunc("/", helloWorld)
+	err := http.ListenAndServe(connHost+":"+connPort, nil)
+	if err != nil {
+		log.Fatal("error starting http server : ", err)
+		return
+	}
+}

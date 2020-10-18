907ab26a09be893fb2e32477e642577a5f4fa0e9
ksitigarbha <58754415+karmicdribble@users.noreply.github.com>
2020-10-18 16:05:40 +0100

    added basic auth

--- http-server-basic-authentication.go
+++ http-server-basic-authentication.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"crypto/subtle"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 const (
-	connHost = "localhost"
-	connPort = "8080"
+	connHost      = "localhost"
+	connPort      = "8080"
+	adminUser     = "admin"
+	adminPassword = "admin"
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
+
+func BasicAuth(handler http.HandlerFunc, realm string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		if !ok || subtle.ConstantTimeCompare([]byte(user),
+			[]byte(adminUser)) != 1 || subtle.ConstantTimeCompare([]byte(pass),
+			[]byte(adminPassword)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+			w.WriteHeader(401)
+			w.Write([]byte("You are unauthorized to access the application.\n"))
+			return
+		}
+		handler(w, r)
+	}
+}
 func main() {
-	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/", BasicAuth(helloWorld, "Please enter your username and password"))
 	err := http.ListenAndServe(connHost+":"+connPort, nil)
 	if err != nil {
 		log.Fatal("error starting http server : ", err)
